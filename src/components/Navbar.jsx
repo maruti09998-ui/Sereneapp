9ccff1ec19f7c8932692f9955e46872ff5cc4023
Maruti <maruti09998@gmail.com>
2026-06-19 21:29:17 +0530

    Update company logo to use new image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,11 +10,7 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="nav-pill logo-pill">
           <Link to="/" className="logo-link">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="3" y="3" width="18" height="18" rx="4" fill="currentColor" fillOpacity="0.2"/>
-              <path d="M7 8h10M7 12h10M7 16h6" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
-            </svg>
-            <span>Serene</span>
+            <img src="/logo.png" alt="Serene Logo" style={{ height: '32px', width: 'auto' }} />
           </Link>
         </div>
 
